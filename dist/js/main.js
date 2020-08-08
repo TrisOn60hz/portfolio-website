e133e81e3df0146f008d59f76aa8db19f213a646
Tristan <tristan@Tristans-MacBook-Pro.local>
2020-08-08 19:32:58 +0700

    did vid 3, added rotating menu btn, doesn't fully work

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+//Select DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".neu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".navItems");
+
+//Set Initial State of Menu
+let showMenu = fales;
+
+menuBtn.addEventListener("click, toggleMenu");
+
+function toggleMenu() {
+  if (!showMenu) {
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    // Set Menu State
+    showMenu = false;
+  }
+}
