c435e2afa489d15e81789d63c8b9b103f9f6f6de
Tristan <tristan@Tristans-MacBook-Pro.local>
2020-08-12 19:24:02 +0700

    fixed menu rotating prooblem

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,9 @@
 //Select DOM Items
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".neu-nav");
+const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".navItems");
+const navItems = document.querySelectorAll(".nav-items");
 
 //Set Initial State of Menu
 let showMenu = false;
